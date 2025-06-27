247e36e56bff56f07f40ad8a785cbabad57da4c9
Password Reset <resetpassservices@gmail.com>
2025-06-27 12:27:16 +0000

    2025-06-27T12:24:43Z [web] Error: [Error: ENOENT: no such file or direct

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 
 import type {NextConfig} from 'next';
-import path from 'path';
 
 const nextConfig: NextConfig = {
   srcDir: 'src',
@@ -21,11 +20,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "50mb",
-    },
-  }
 };
 
 export default nextConfig;
